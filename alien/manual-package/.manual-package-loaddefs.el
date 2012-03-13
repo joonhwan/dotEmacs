722506a5c1e1dc59cb72b4698d86cdb3792e430d
Joonhwan Lee <jhlee@jhlee.local>
2012-03-14 01:11:00 +0900

    learned how to use 'update-directory-autoloads' and its output file. :)

---
+++ alien/manual-package/.manual-package-loaddefs.el
@@ -0,0 +1,32 @@
+;;; .loaddefs.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (plantuml-mode) "plantuml-mode" "plantuml-mode.el"
+;;;;;;  (20319 26563))
+;;; Generated autoloads from plantuml-mode.el
+
+(autoload 'plantuml-mode "plantuml-mode" "\
+Major mode for plantuml.
+
+Shortcuts             Command Name
+\\[plantuml-complete-symbol]      `plantuml-complete-symbol'
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("cmake-mode.el" "org-todoledo.el") (20319
+;;;;;;  26675 540055))
+
+;;;***
+
+(provide '.loaddefs)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; .loaddefs.el ends here
