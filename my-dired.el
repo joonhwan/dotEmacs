10a61c6e337d35cc82aa77c59cd5dfd25a392b32
Joonhwan <joonhwan.lee@gmail.com>
2012-11-30 08:37:55 +0900

    fix dired-mode display error(find-dired do not display sub directories.)

--- my-dired.el
+++ my-dired.el
@@ -47,50 +47,50 @@
 ;;
 ;; hack find-lisp-dired
 ;;
-(defun my-find-lisp-format (file-name file-attr switches now)
-  (let ((file-type (nth 0 file-attr)))
-    (concat (if (memq ?i switches)	; inode number
-		(format "%6d " (nth 10 file-attr)))
-	    ;; nil is treated like "" in concat
-	    (if (memq ?s switches)	; size in K
-		(format "%4d " (1+ (/ (nth 7 file-attr) 1024))))
-	    (nth 8 file-attr)		; permission bits
-	    (format " %3d %-8s %-8s %8d "
-		    (nth 1 file-attr)	; no. of links
-		    (if (numberp (nth 2 file-attr))
-			(int-to-string (nth 2 file-attr))
-		      (nth 2 file-attr)) ; uid
-		    (if (eq system-type 'ms-dos)
-			"root"		; everything is root on MSDOS.
-		      (if (numberp (nth 3 file-attr))
-			  (int-to-string (nth 3 file-attr))
-			(nth 3 file-attr))) ; gid
-		    (nth 7 file-attr)	; size in bytes
-		    )
-	    (find-lisp-format-time file-attr switches now)
-	    " "
-	    file-name
-	    (if (stringp file-type)	; is a symbolic link
-		(concat " -> " file-type)
-	      "")
-	    "\n")))
-(if (functionp 'find-lisp-format)
-	(fset 'find-lisp-format 'my-find-lisp-format))
-;; (defun my-find-lisp-format-time (file-attr switches now)
-;;   ;; Format time string for file with attributes FILE-ATTR according
-;;   ;; to SWITCHES (a list of ls option letters of which c and u are recognized).
-;;   ;; Use the same method as `ls' to decide whether to show time-of-day or year,
-;;   ;; depending on distance between file date and NOW.
-;;   (let* ((time (nth (find-lisp-time-index switches) file-attr))
-;; 	 (diff16 (- (car time) (car now)))
-;; 	 (diff (+ (ash diff16 16) (- (car (cdr time)) (car (cdr now)))))
-;; 	 (past-cutoff (- (* 6 30 24 60 60)))	; 6 30-day months
-;; 	 (future-cutoff (* 60 60)))		; 1 hour
-;;     (format-time-string
-;;      "%Y/%_2m/%e %2H:%2M
-;;      time)))))
-;; (if (functionp 'find-lisp-format-time)
-;; 	(fset 'find-lisp-format-time 'my-find-lisp-format-time))
+;; (defun my-find-lisp-format (file-name file-attr switches now)
+;;   (let ((file-type (nth 0 file-attr)))
+;;     (concat (if (memq ?i switches)	; inode number
+;; 		(format "%6d " (nth 10 file-attr)))
+;; 	    ;; nil is treated like "" in concat
+;; 	    (if (memq ?s switches)	; size in K
+;; 		(format "%4d " (1+ (/ (nth 7 file-attr) 1024))))
+;; 	    (nth 8 file-attr)		; permission bits
+;; 	    (format " %3d %-8s %-8s %8d "
+;; 		    (nth 1 file-attr)	; no. of links
+;; 		    (if (numberp (nth 2 file-attr))
+;; 			(int-to-string (nth 2 file-attr))
+;; 		      (nth 2 file-attr)) ; uid
+;; 		    (if (eq system-type 'ms-dos)
+;; 			"root"		; everything is root on MSDOS.
+;; 		      (if (numberp (nth 3 file-attr))
+;; 			  (int-to-string (nth 3 file-attr))
+;; 			(nth 3 file-attr))) ; gid
+;; 		    (nth 7 file-attr)	; size in bytes
+;; 		    )
+;; 	    (find-lisp-format-time file-attr switches now)
+;; 	    " "
+;; 	    file-name
+;; 	    (if (stringp file-type)	; is a symbolic link
+;; 		(concat " -> " file-type)
+;; 	      "")
+;; 	    "\n")))
+;; (if (functionp 'find-lisp-format)
+;; 	(fset 'find-lisp-format 'my-find-lisp-format))
+;; ;; (defun my-find-lisp-format-time (file-attr switches now)
+;; ;;   ;; Format time string for file with attributes FILE-ATTR according
+;; ;;   ;; to SWITCHES (a list of ls option letters of which c and u are recognized).
+;; ;;   ;; Use the same method as `ls' to decide whether to show time-of-day or year,
+;; ;;   ;; depending on distance between file date and NOW.
+;; ;;   (let* ((time (nth (find-lisp-time-index switches) file-attr))
+;; ;; 	 (diff16 (- (car time) (car now)))
+;; ;; 	 (diff (+ (ash diff16 16) (- (car (cdr time)) (car (cdr now)))))
+;; ;; 	 (past-cutoff (- (* 6 30 24 60 60)))	; 6 30-day months
+;; ;; 	 (future-cutoff (* 60 60)))		; 1 hour
+;; ;;     (format-time-string
+;; ;;      "%Y/%_2m/%e %2H:%2M
+;; ;;      time)))))
+;; ;; (if (functionp 'find-lisp-format-time)
+;; ;; 	(fset 'find-lisp-format-time 'my-find-lisp-format-time))
 
 ;; 
 ;; dired setting
@@ -127,7 +127,7 @@
 ;; (define-key dired-mode-map [(meta return)] 'my-open-in-total-cmd)
 
 (defun my-dired-mode-hook ()
-  (dired-omit-mode -1)
+  ;; (dired-omit-mode 1)
   (subword-mode 1)
   )
 
