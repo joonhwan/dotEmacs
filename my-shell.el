a3992b0a0458fc4dbed9872723e9736017880ea7
Joonhwan <joonhwan.lee@gmail.com>
2013-01-02 17:51:37 +0900

    improved C-z "shell" command to goto current directory if prefixed

--- my-shell.el
+++ my-shell.el
@@ -1,4 +1,22 @@
-(global-set-key "\C-z" 'shell)
+(defun my-get-existing-shell-buffer ()
+  (let (proc proc-buffer)
+	(catch 'break
+	  (dolist (proc (process-list))
+		(if (string= (process-name proc) "shell")
+			(throw 'break (setq proc-buffer (process-buffer proc))))))
+	))
+
+(defun my-shell-with-current-directory (&optional arg)
+  (interactive "P")
+  (let* ((sp (get-process "shell"))
+		 (dir (if buffer-file-name (file-name-directory buffer-file-name) default-directory)))
+	(if (and arg sp dir)
+		(progn 
+		  (comint-simple-send sp (concat "cd " dir))
+		  (display-buffer (process-buffer sp)))
+	  (shell))))
+		 
+(global-set-key "\C-z" 'my-shell-with-current-directory)
 
 (defun my-shell-setup()
   (dirtrack-mode t)
